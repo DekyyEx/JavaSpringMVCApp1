33b693fe39521141f7100ffe5e69645f4505ddf1
DekyyEx <vlad.moor123@mail.ru>
2024-03-31 17:41:32 +0300

    working project

--- src/main/java/ru/alishev/springcourse/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.alishev.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,15 +16,32 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        logger.info("Hello, " + name + " " + surname);
-
+//        logger.info("Hello, " + name + " " + surname);
+        model.addAttribute("massage", "Hello, " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodbyePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a, @RequestParam("b") int b,
+                             @RequestParam("action") String action, Model model) {
+        double result = switch (action) {
+            case "multiplication" -> a * b;
+            case "division" -> a / (double) b;
+            case "subtraction" -> a - b;
+            case "addition" -> a + b;
+            default -> 0;
+        };
+
+        model.addAttribute("result", result);
+
+        return "first/calculator";
     }
 }
